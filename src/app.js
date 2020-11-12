50ae4f1a69c412ce9524b975ef434b5963836b77
jairoerazobotero <jairoerazo@hotmail.com>
2020-11-12 14:58:14 +0100

    send message alert

--- src/app.js
+++ src/app.js
@@ -7,13 +7,23 @@ const config = require('./config');
 async function startServer(){
   const app = new Koa();
   require('./loaders/mongo')(app);
-  require('./loaders/mqtt')(app);
   app
     .use(bodyParser())
     .use(cors())
     .use(routes.getEnvironment)
     .use(routes.security);
-  app.listen(config.port)
+  server = app.listen(config.port);
+
+  // socket connection
+  const options={
+    cors:true,
+    origins:['http://localhost:8080'],
+   };
+  const io = require('socket.io')(server, options);
+  require('./loaders/mqtt')(app, io);
+  io.on('connection', (socket) => {
+    console.log('Socket created')
+  });
 }
 
 startServer();
--- src/loaders/mqtt.js
+++ src/loaders/mqtt.js
@@ -5,7 +5,7 @@ const options = {
   password: config.thingyPass
 };
 
-module.exports = function (app) {
+module.exports = function (app, io) {
   const thingyClient = mqtt.connect(config.mqttURL, options);
   thingyClient.on('connect', () => {
     thingyClient.subscribe('things/brown-3/shadow/update', () => {
@@ -17,6 +17,7 @@ module.exports = function (app) {
     app.message = JSON.parse(message.toString());
     if(app.message.appId === 'BUTTON') {
       console.log('BUTTON');
+      io.emit('ALARM', { alarm: true})
     }
     if(app.message.appId === 'TEMP') {
       app.temperature = JSON.parse(message.toString());
